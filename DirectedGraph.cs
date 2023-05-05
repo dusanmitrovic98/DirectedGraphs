7901c8645f6e6ac55fea94b8b7ce0728145d1039
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-05 23:30:39 +0200

    Removed method.

--- DirectedGraph.cs
+++ DirectedGraph.cs
@@ -36,13 +36,4 @@ public class DirectedGraph
         return this._nodes.Count;
     }
 
-    public List<int> GetNeighbors(int nodeId)
-    {
-        if (this._nodes.ContainsKey(nodeId))
-        {
-            return this._nodes[nodeId].Edges.Select(edge => edge.EndNode).ToList();
-        }
-
-        return new List<int>();
-    }
-}
+   
