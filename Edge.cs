6e9116627127a2d08768495e9d599c862529eac5
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-05 22:57:42 +0200

    Added StartNode property.

--- Edge.cs
+++ Edge.cs
@@ -4,4 +4,10 @@ public class Edge
 {
     private int _startNode;
     private int _endNode;
+
+    public int StartNode
+    {
+        get { return _startNode; }
+        set { _startNode = value; }
+    }
 }
