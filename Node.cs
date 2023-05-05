44e06a258038ef0ffb0269afec37d3a2c6e33790
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-05 23:03:17 +0200

    Added Id property.

--- Node.cs
+++ Node.cs
@@ -3,4 +3,10 @@ namespace DataStructures.Collections.Graphs;
 public class Node
 {
     private int _id;
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
 }
