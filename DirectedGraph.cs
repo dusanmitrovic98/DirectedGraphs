c836b9b35c5cd6dd7bc78ac97cec16b980ba6fe6
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-05 23:18:06 +0200

    Added Nodes property.

--- DirectedGraph.cs
+++ DirectedGraph.cs
@@ -3,4 +3,10 @@
 public class DirectedGraph
 {
     private Dictionary<int, Node> _nodes;
+
+    public Dictionary<int, Node> Nodes
+    {
+        get { return this._nodes; }
+        set { this._nodes = value; }
+    }
 }
